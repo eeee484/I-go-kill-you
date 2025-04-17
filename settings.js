684c5c0dde770024637fed6d1a91cf9a3154db6a
eeee484 <inventoremmanuel44@gmail.com>
2025-04-17 23:49:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "your phone go die",
   botOwner: 'Emmanuel', // Your name
-  ownerNumber: '14703105589', //Your number
+  ownerNumber: '8148448338', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
